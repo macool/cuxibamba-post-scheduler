48126696e142e6682afe05406835300113976946
Mario Andrés Correa <a@macool.me>
2017-05-25 01:22:05 -0500

    set errbit host again

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,4 @@
 Airbrake.configure do |config|
   config.api_key = 'c30f7aacdbce66cc9d04f617d42164f0'
   config.host    = 'http://pangi.shiriculapo.com'
-  config.port    = 80
-  config.secure  = config.port == 443
 end
