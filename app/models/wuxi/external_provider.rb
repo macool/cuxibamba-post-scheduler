a3818a8137e82605b97c1683d6b046131b92cf97
Mario Andrés Correa <a@macool.me>
2017-05-25 01:38:15 -0500

    reasonable cache time

--- app/models/wuxi/external_provider.rb
+++ app/models/wuxi/external_provider.rb
@@ -1,6 +1,6 @@
 class Wuxi::ExternalProvider < Wuxi::BaseResource
   unless Rails.env.development?
-    cached_resource collection_synchronize: true
+    cached_resource collection_synchronize: true, ttl: 1800 # 30 mins
   end
 
   def self.active_for_api
