3f9e1aed29e5a5732fc2263a39facd227352ee48
Mario Andrés Correa <a@macool.me>
2017-05-25 01:26:54 -0500

    darn

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,4 +1,4 @@
 Airbrake.configure do |config|
   config.api_key = 'c30f7aacdbce66cc9d04f617d42164f0'
-  config.host    = 'http://pangi.shiriculapo.com'
+  config.host    = 'pangi.shiriculapo.com'
 end
