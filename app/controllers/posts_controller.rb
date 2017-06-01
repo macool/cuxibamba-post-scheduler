0fd53feaba6277c1e4b5706aa2d8006cb9cb12b4
Mario Andrés Correa <a@macool.me>
2017-06-01 10:45:04 -0500

    fuck me

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.new(
-      policy_scope(Post).permitted_attributes
+      policy(Post).permitted_attributes
     )
     if post_policy.create? && @post.save
       flash[:success] = t("ui.post.created", date: @post.share_at.to_s)
