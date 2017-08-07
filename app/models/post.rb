9c1f777064293fdc9176413a909afd064fb8fa2a
Mario Andrés Correa <a@macool.me>
2017-08-06 21:24:56 -0500

    tweaking UI

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,7 +17,7 @@ class Post
   field :banner, type: String
   field :auto_follow_link, type: Boolean
   field :highlight, type: Boolean, default: true
-  field :format_md, type: Boolean, default: true
+  field :format_md, type: Boolean, default: false
 
   belongs_to :user
   has_many :post_visits
